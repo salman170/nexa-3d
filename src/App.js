3aff63759b1c950171a3de2ff3d1eb897a6c0c2d
salman170 <salmanansari02120@gmail.com>
2024-01-03 11:36:01 +0530

    applied color

--- src/App.js
+++ src/App.js
@@ -1,30 +1,102 @@
-
 import { Canvas } from "@react-three/fiber";
-import {
-  // Center,
-  // AccumulativeShadows,
-  // RandomizedLight,
-  OrbitControls,
-  Environment,
-} from "@react-three/drei";
+
 import { XLThree } from "./screen/XL6/XLThree";
+import { useControls, button } from "leva";
 import { Suspense } from "react";
 
+import {
+  Center,
+  AccumulativeShadows,
+  RandomizedLight,
+  Environment,
+  OrbitControls,
+  ContactShadows,
+  Lightformer,
+} from "@react-three/drei";
 
 export default function App() {
+  const { color, realism, importanceSampling } = useControls({
+    color: "#ff9621",
+    // realism: true,
+    // importanceSampling: true,
+    screenshot: button(() => {
+      const link = document.createElement("a");
+      link.setAttribute("download", "canvas.png");
+      link.setAttribute(
+        "href",
+        document
+          .querySelector("canvas")
+          .toDataURL("image/png")
+          .replace("image/png", "image/octet-stream")
+      );
+      link.click();
+    }),
+  });
+
   return (
-    <Canvas shadows camera={{ position: [4, 2.5, 8], fov: 35 }}>
-      <color attach="background" args={['#15151a']} />
-      <spotLight position={[0, 15, 0]} angle={0.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
-      <ambientLight intensity={0.5} />
-      <Suspense fallback={null}>
-        <XLThree />
-        <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2}   />
-        <Environment preset="city" />
-        {/* <Environment files="https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/hdris/noon-grass/noon_grass_1k.hdr" background /> */}
-      </Suspense>
+   
+    <Canvas
+      gl={{ antialias: false, preserveDrawingBuffer: true }}
+      shadows
+      camera={{ position: [4, 0, 6], fov: 35 }}
+    >
+      <color attach="background" args={["#15151a"]} />
+      <group position={[0, -1.2, 0]}>
+        <Center top>
+          <XLThree color={color}  />
+        </Center>
+        <AccumulativeShadows>
+          <RandomizedLight position={[2, 5, 5]} />
+        </AccumulativeShadows>
+      </group>
+      <hemisphereLight intensity={0.5} />
+      <ContactShadows
+        resolution={1024}
+        frames={1}
+        position={[0, -1.16, 0]}
+        scale={15}
+        blur={0.5}
+        opacity={1}
+        far={20}
+      />
+      <mesh
+        scale={2}
+        position={[2, -1.161, -1.5]}
+        rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}
+      >
+        <ringGeometry args={[0.9, 1, 4, 1]} />
+        <meshStandardMaterial color="white" roughness={0.75} />
+      </mesh>
+      <mesh
+        scale={2}
+        position={[-2, -1.161, -1]}
+        rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}
+      >
+        <ringGeometry args={[0.9, 1, 3, 1]} />
+        <meshStandardMaterial color="white" roughness={0.75} />
+      </mesh>
+      <OrbitControls
+        makeDefault
+        minPolarAngle={0}
+        maxPolarAngle={Math.PI / 2}
+      />
+      {/* {realism && <Effects importanceSampling={importanceSampling} />} */}
+      {/* <Environment preset="dawn" background blur={1} /> */}
+      <Environment resolution={512}>
+        {/* Ceiling */}
+        <Lightformer intensity={6} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} />
+        <Lightformer intensity={6} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} />
+        <Lightformer intensity={6} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} />
+        <Lightformer intensity={6} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} />
+        <Lightformer intensity={6} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} />
+        <Lightformer intensity={6} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
+        <Lightformer intensity={6} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
+        {/* Sides */}
+        <Lightformer intensity={6} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
+        <Lightformer intensity={6} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
+        {/* Key */}
+        {/* <Lightformer form="ring" color="red" intensity={10} scale={2} position={[10, 5, 10]} onUpdate={(self) => self.lookAt(0, 0, 0)} /> */}
+      </Environment>
     </Canvas>
   );
 }
-
-
